60b060137c8fce47784f28339f882de8205c4ab4
raonircl <raoni.afi@gmail.com>
2022-10-25 03:41:24 -0300

    adding characters limits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,6 @@ class UsersController < ApplicationController
       render json: @user.errors, status: :unprocessable_entity
     end
   end
-  
-  def index
-    @users = User.all
-    render json: @users
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
